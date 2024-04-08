9a191df14fa13ae5add278edf038ed0f798adbdc
Pavan Shanmukha Madhav Gunda <82692757+pand-coder@users.noreply.github.com>
2024-04-08 22:37:24 +0530

    Create Datatypes.js

---
+++ Basics/Datatypes.js
@@ -0,0 +1,24 @@
+"Use Strict";
+//alert("Hello Bhai" //using nodejs not browser's console//
+console.log(4+5);
+ console.log("pavan")
+ //Primitive Datatypes//
+let name='pavan';//string//
+let age=18;//number//
+let isloggedin=false;//bool//
+ //number -> 2 to power of 53
+ //use bigint for lsrge nos//
+ //for string use " " u can also use ' '//
+ //boolean -> true/false//
+ // null -> standalone value (object)//
+ let state;
+ //any variable whose value is not assigned is undefined//
+ let s1=null;
+ console.log(s1);
+ //undefined -> 
+ //symbol -> unique//
+ console.log(typeof(s1))
+ console.log(typeof(console))
+ console.log(typeof(null))  //null//
+ console.log(typeof(undefined)) //undefined//
+
