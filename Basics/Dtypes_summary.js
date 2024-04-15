5832265fa7fac31d5703a2de4bca7a923f302f50
Pavan Shanmukha Madhav Gunda <82692757+pand-coder@users.noreply.github.com>
2024-04-15 23:46:17 +0530

    Create Dtypes_summary.js

---
+++ Basics/Dtypes_summary.js
@@ -0,0 +1,33 @@
+/*
+JavaScript is a dynamically typed language. Because data type will automatically assigned at the time of compilation or code execution.
+Primitive Datatypes(Call by value)
+7 types
+-------
+String
+Number
+Boolean
+null
+undefined
+Symbol
+BigInt
+
+Non-Primitive Datatypes (Call by reference)
+Arrays
+Objects
+Functions
+
+*/
+
+const score=100;
+const scored=1.08970;
+const isloggedin=false;
+const outsidetemp=null;
+let u;
+
+const rollnuber=Symbol('22049')
+const rollnumber=Symbol('22049')
+console.log(rollnuber===rollnumber);
+
+
+
+
