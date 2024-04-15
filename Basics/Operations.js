ff6c53a5aa27c4b293fd89c1534b45e299bd3edb
Pavan Shanmukha Madhav Gunda <82692757+pand-coder@users.noreply.github.com>
2024-04-15 23:45:48 +0530

    Update and rename conversion_operations.js to Operations.js

---
+++ Basics/Operations.js
@@ -0,0 +1,29 @@
+let val=3;
+let negval=-val;
+//console.log(typeof negval)
+//console.log(2+2);
+//console.log(2-2);
+//console.log(2*2);
+//console.log(2/2);
+//console.log(2%2);
+//console.log(2**2);
+let str1="Pavan "
+let str2="Shanmukha Madhav Gunda"
+//console.log(str1+str2);
+
+//console.log("4"+9);
+//console.log(4+"9");
+//console.log("1"+2+2);
+//If there is a first occurence of string then every thing will be converted as string//
+//If there is a string at last then everything will be converted as which datatype occurence//
+console.log(+true);
+console.log(+"");
+
+let n1,n2,n3;
+n1=n2=n3=2+2;
+console.log(n1,n2,n3);
+
+let gc=100;
+gc++
+console.log(gc);
+
